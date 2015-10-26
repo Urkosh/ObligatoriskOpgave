8bd4daa6c44d583cdeebd9d6e5a00f9838721dfb
Urkosh <uncleurkosh@gmail.com>
2015-10-26 13:43:21 +0100

    Starten på et listview til forbedret træ struktur

--- app/src/main/java/poulsen/obligatoriskopgvae/LoggedInActivity.java
+++ app/src/main/java/poulsen/obligatoriskopgvae/LoggedInActivity.java
@@ -1,18 +1,39 @@
 package poulsen.obligatoriskopgvae;
 
+import android.database.DataSetObserver;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ListAdapter;
+import android.widget.ListView;
+
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.Arrays;
 
 import poulsen.obligatoriskopgvae.R;
 
 public class LoggedInActivity extends AppCompatActivity {
 
+    private ListView listView;
+    private ArrayAdapter<String> listAdapter;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_logged_in);
+
+        listView = (ListView) findViewById(R.id.listView);
+        final String [] questionsArray = getResources().getStringArray(R.array.Questions);
+        ArrayList<String> questionList = new ArrayList<String>();
+        questionList.addAll(Arrays.asList(questionsArray));
+
+        listAdapter = new ArrayAdapter<String>(this, R.layout.row_layout, questionList);
+        listView.setAdapter(listAdapter);
     }
 
     @Override
